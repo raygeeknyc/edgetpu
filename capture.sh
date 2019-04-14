bd5c601037c1fbee4aed414b77a41c14072af976
Raymond Blum <raygeeknyc@gmail.com>
2019-04-14 07:24:53 -0400

    Added video capture demo

---
+++ capture.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+time python3 /usr/local/lib/python3.5/dist-packages/edgetpu/demo/classify_capture.py --model ./resources/mobilenet_v2_1.0_224_quant_edgetpu.tflite --label ./resources/imagenet_labels.txt
