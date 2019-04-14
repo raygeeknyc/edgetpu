8a4a710cc4a95babb69701be408140563dfbeea2
Raymond Blum <raygeeknyc@gmail.com>
2019-04-14 07:26:19 -0400

    Added note

--- capture.sh
+++ capture.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# output only goes to HDMI not VNC
 time python3 /usr/local/lib/python3.5/dist-packages/edgetpu/demo/classify_capture.py --model ./resources/mobilenet_v2_1.0_224_quant_edgetpu.tflite --label ./resources/imagenet_labels.txt
